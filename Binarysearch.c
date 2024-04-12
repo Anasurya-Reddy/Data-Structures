65760d929bc70a5c60043cc3916d51850d1d774b
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-04-12 20:51:47 +0530

    Update Binarysearch.c

--- Binarysearch.c
+++ Binarysearch.c
@@ -21,9 +21,13 @@ int main() {
 int binarysearch (int low, int high)
 {
     int mid;
-    if(low>high) return(-1);
+    if(low>high) 
+        return(-1);
     mid=(low+high)/2;
-    if(key==a[mid]) return(mid);
-    else if(key<a[mid]) return(bsearch(low,mid-1));
-    else return(bsearch(mid+1,high));
+    if(key==a[mid]) 
+        return(mid);
+    else if(key<a[mid]) 
+        return(bsearch(low,mid-1));
+    else 
+        return(bsearch(mid+1,high));
 }

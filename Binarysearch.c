5e4a9bc251cedf756bd0b40987ec18651005af45
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-04-13 22:07:24 +0530

    Update Binarysearch.c

--- Binarysearch.c
+++ Binarysearch.c
@@ -1,22 +1,20 @@
+/*C program to perform Binary search*/
 #include <stdio.h>
-void search(int *,int ,int );
+void binarysearch(int *,int ,int );
 int main()
 {
     int a[20],n,i,key;
     printf("Enter the list size:");
     scanf("%d",&n);
-    printf("Enter the elements in the list:");
+    printf("Enter the elements in the list:\n");
     for(i=0;i<n;i++)
-    {
         scanf("%d",&a[i]);
-    }
-    printf("Enter the element to be searched:");
+    printf("Enter the key:");
     scanf("%d",&key);
-    search(a,n,key);
+    binarysearch(a,n,key);
     return 0;
 }
-
-void search(int *a,int n,int key)
+void binarysearch(int *a,int n,int key)
 {
     int low,mid,high,count=0;
     low=0;
@@ -26,18 +24,14 @@ void search(int *a,int n,int key)
         mid=(low+high)/2;
         if(key==a[mid])
         {
-            printf("Search is succesful,Key is found at index %d",mid);
+            printf("Key is found at index:%d",mid);
             count=1;
             break;
         }
-        else if(key<mid)
-        {
+        else if(key<a[mid])
             high=mid-1;
-        }
         else
-        {
             low=mid+1;
-        }
     }
     if(count==0)
         printf("Search is unsuccesful");

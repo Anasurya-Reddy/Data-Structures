276d5a5dad323f01d64b735f939e8ae708cdce32
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-04-13 22:09:04 +0530

    Update Bubblesort.c

--- Bubblesort.c
+++ Bubblesort.c
@@ -1,25 +1,25 @@
 /*C Program that implements Bubble sort */
 #include<stdio.h>
-void bubblesort (int[], int);
-void main()
+void bubblesort (int *, int);
+int main()
 {
-    int a[50], i, n;
-    printf("Enter n:");
+    int a[20], i, n;
+    printf("Enter the size of list:");
     scanf("%d", &n);
-    printf("Enter array elements:");
+    printf("Enter the elements:\n");
     for(i=0; i<n; i++)
         scanf("%d", &a[i]);
     bubblesort(a,n);
-    printf("Sorted list\n");
+    printf("Sorted list:\n");
     for(i=0; i<n; i++)
-        printf("%5d", a[i]);
+        printf("%d ", a[i]);
 }
-void bubblesort(int a[], int n)
+void bubblesort(int *a, int n)
 {
     int i, j, temp;
-    for(i=0; i<=(n-2); i++)
+    for(i=0; i<n-1; i++)
     {
-        for(j=0; j<=(n-2-i); j++)
+        for(j=0; j<(n-1-i); j++)
         {
             if(a[j] > a[j+1])
             {
